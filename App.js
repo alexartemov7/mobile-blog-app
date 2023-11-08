bc0b45c196fc6480b2497b23b0502288c402f920
Alex Artemov <artemov.sandro@gmail.com>
2023-11-08 13:32:13 -0500

    created an Intagram-like blog app hitting pre-built API with MongoDB connection

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image, ScrollView, SafeAreaView } from 'react-native';
+import PostCard from './components/PostCard';
 
 export default function App() {
+  const [blogPosts, setBlogPosts] = useState([])
+  //useEffect is being called on different lifecycles of the component
+  // 1. When component mounts
+  // 2. When component unmounts
+
+  const handleFetchData = () => {
+    // console.log('fetching data')
+
+  }
+  useEffect(() => {
+    fetch('http://192.168.10.239:8080')
+      .then(res => res.json())
+      .then(data => setBlogPosts(data))
+      .catch(err => console.error(err))
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+
+      <View>
+      <ScrollView>
+        {blogPosts.map((singlePost, index) => {
+          console.log(singlePost)
+          return (
+            <PostCard singlePost={singlePost} index={index} key={singlePost._id}/>
+          )
+        })}
+      </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
